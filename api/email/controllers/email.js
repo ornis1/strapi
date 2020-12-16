06f5833ba98f2927bd5e73cdb67e5f9a5058a8e1
margarita-egorycheva <margarita.egorycheva@gmail.com>
2020-12-16 21:34:34 +0300

    update email send controller, remove trash

--- api/email/controllers/email.js
+++ api/email/controllers/email.js
@@ -2,8 +2,8 @@
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /* Существуют ли нужные поля */
-const isData = ({ email, products, fullname }) => {
-  return email && products && fullname;
+const isData = ({ phone, products, fullname }) => {
+  return phone && products && fullname;
 };
 
 module.exports = {
@@ -16,10 +16,15 @@ module.exports = {
     const data = ctx.request.body;
 
     if (!isData(data)) {
-      return "Error, invalid order data";
+      return { error: true, message: "Invalid order data" };
     }
 
-    const { products: queryProducts, email, fullname } = data;
+    const {
+      products: queryProducts,
+      phone,
+      fullname = "",
+      additional = "",
+    } = data;
     const ids = queryProducts.map((p) => p.id);
 
     /* Поиск продуктов в бд для добавления их в заказ */
@@ -38,7 +43,8 @@ module.exports = {
     /* Добавляем заказ в бд */
     const createdOrder = await strapi.services.orders.create({
       amount,
-      email,
+      phone,
+      additional,
       fullname,
       products: ordersProducts,
     });
@@ -46,17 +52,26 @@ module.exports = {
     const domain = process.env.DOMAIN
       ? `https://${process.env.DOMAIN}/api`
       : "http://localhost:1337";
-    const list = ordersProducts.map((p, index) => {
-      return `
-      <p>${index + 1}) ${p.name} ${getQty(+p.id)}шт.</p>
-      <p>На сумму ${amount}руб.</p>
-      `;
-    });
+
+    const list = ordersProducts
+      .map((p, index) => {
+        return `<p>${index + 1}) ${p.name} ${getQty(+p.id)}шт.</p>`;
+      })
+      .join("");
+
+    const additionalMarkup = additional
+      ? `<p>Пожелания к заказу: ${additional}</p>`
+      : "";
+
     const message = `<h1>Новый заказ  id: ${createdOrder.id}</h1>
+    <p>От ${fullname}</p>
+    <p>${phone}</p>
+    ${additional ? additionalMarkup : ""}
+
     <h3>Список</h3>
     ${list}
-    <a href="${domain}/admin/plugins/content-manager/collectionType/application::orders.orders">Тык</a>
-    `;
+    <p>На сумму ${amount}руб.</p>
+    <a href="${domain}/admin/plugins/content-manager/collectionType/application::orders.orders">Тык</a>`;
 
     /* Отправляем на почту уведомление о новом заказе */
     /* Почту на которую отправляем уведомление берем из бд */
@@ -80,6 +95,6 @@ module.exports = {
       })
       .catch((e) => console.error(e.response.body.errors));
 
-    return "";
+    return { error: false, message: "success" };
   },
 };
