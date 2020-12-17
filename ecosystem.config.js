8c6b6a547bf35f2d9064e724b76e42100c61622c
margarita-egorycheva <margarita.egorycheva@gmail.com>
2020-12-17 18:30:46 +0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "back",
+      name: "workflow",
       script: "npm",
       args: "run start",
       watch: ".",
-      // mode: "cluster",
-      // instances: 2,
-      watch: ["config", "components", "api", "ecosystem.config.js"],
-      env: {
-        DOMAIN: "104.248.52.14",
-        NODE_ENV: "production",
-        PORT: 1337,
-        HOST: "127.0.0.1",
-        DATABASE_HOST: "localhost",
-        DATABASE_PORT: "5432",
-        DATABASE_NAME: "strapi",
-        DATABASE_USERNAME: "strapi",
-        DATABASE_PASSWORD: "",
-      },
+      watch: ["config", "components", "api"],
     },
   ],
 };
