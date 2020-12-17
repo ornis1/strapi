98447ef8578db525ddaf077704d861398021d50e
margarita-egorycheva <margarita.egorycheva@gmail.com>
2020-12-17 20:28:13 +0300

    test

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -1,16 +1,21 @@
-module.exports = ({ env }) => ({
-  email: {
-    provider: "sendgrid",
-    providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
+module.exports = ({ env }) => {
+  console.log(env("CLOUDINARY_NAME"));
+  console.log(env("CLOUDINARY_KEY"));
+  console.log(env("CLOUDINARY_SECRET"));
+  return {
+    email: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
     },
-  },
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
     },
-  },
-});
+  };
+};
